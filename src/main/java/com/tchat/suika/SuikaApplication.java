1d6e79705e178731f8d29d8dea107cfdd9bb9e1b
allan <allanduminy@protonmail.com>
2023-11-30 15:11:06 +0100

    init project

---
+++ src/main/java/com/tchat/suika/SuikaApplication.java
@@ -0,0 +1,13 @@
+package com.tchat.suika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuikaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuikaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tchat/suika/SuikaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tchat.suika;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuikaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
